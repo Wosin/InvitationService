3673d2e67800f064229834121bb828ca88de536e
Dominik Wosinski <wossyn@gmail.com>
2017-09-05 15:59:57 +0200

    Added possibility to set host and port via args

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -2,11 +2,43 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import routes.InvitationRoute
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
-object Boot extends App with InvitationRoute {
-  implicit val system: ActorSystem = ActorSystem("InvitationRestService")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  val binding = Http().bindAndHandle(invitationServiceRoute, "localhost", 8080)
+object Boot extends InvitationRoute {
+
+   def main(args: Array[String]): Unit = {
+    println("Run run.sh [host] [port] to override default host and port")
+    val defaultPort = 8080
+    val defaultHost = "localhost"
+    var userDefinedPort:Option[Int] = Option.empty
+    var userDefinedHost:Option[String] = Option.empty
+     args foreach(arg =>println(arg))
+    args.length match  {
+      case 2 => {
+        println(s"Using user defined host: ${args(0)}")
+        userDefinedHost = Option(args(0))
+        println(s"Using user defined port: ${args(1)}")
+        userDefinedPort = Option(args(1).toInt)
+      }
+      case _ => print("Using default host: localhost and port: 8080")
+
+      }
+
+
+    val applicationPort:Int = if(userDefinedPort.isEmpty) defaultPort else userDefinedPort.get
+    val applicationHost:String = if(userDefinedHost.isEmpty) defaultHost else userDefinedHost.get
+    implicit val system: ActorSystem = ActorSystem("InvitationRestService")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    val binding = Http().bindAndHandle(invitationServiceRoute, applicationHost, applicationPort)
+    binding.onComplete {
+      case Success(_) =>
+        println(s"Server started succesfully and is listening on $applicationHost:$applicationPort")
+      case Failure(reason) =>
+        println("Failed to start server. Reason:", reason)
+    }
+
+  }
 
 }
 
