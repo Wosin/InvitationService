b6c1ffb22f0dfb8c66fd9809ddde81f34b4c99c0
Dominik Wosinski <wossyn@gmail.com>
2017-09-05 11:43:13 +0200

    Added Json Marshalling of Invitation class

--- src/main/scala/model/Invitation.scala
+++ src/main/scala/model/Invitation.scala
@@ -1,3 +1,9 @@
 package model
 
+import spray.json.DefaultJsonProtocol
+
 case class Invitation(invitee: String, email: String)
+
+object Invitation extends DefaultJsonProtocol {
+  implicit val invitationFormat = jsonFormat2(Invitation.apply)
+}
