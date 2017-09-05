408142d188a96b9b66a20ec94a9f42b5b9326486
Dominik Wosinski <wossyn@gmail.com>
2017-09-05 13:04:41 +0200

    Tests for POST added + refactor

--- src/test/scala/InvitationServiceTest.scala
+++ src/test/scala/InvitationServiceTest.scala
@@ -5,16 +5,16 @@ import akka.http.scaladsl.unmarshalling.Unmarshal
 import model.Invitation
 import org.scalatest.{Matchers, WordSpec}
 import routes.InvitationRoute
+import spray.json.pimpAny
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
-class InvitationServiceTest extends WordSpec with Matchers with ScalatestRouteTest with InvitationRoute{
-  "Rest Interface" should {
-    "Respond healthy " in {
+class InvitationServiceTest extends WordSpec with Matchers with ScalatestRouteTest with InvitationRoute {
+  "REST Interface" should {
+    "GET Healthcheck should respond with healthy " in {
 
       val getHealthy = HttpRequest(HttpMethods.GET, uri = "/healthcheck")
-
 
 
       getHealthy ~> invitationServiceRoute ~> check {
@@ -25,7 +25,8 @@ class InvitationServiceTest extends WordSpec with Matchers with ScalatestRouteTe
         returnedData shouldEqual "Alive & Healthy"
       }
     }
-    "Respond with empty List" in {
+
+    "GET Invitations should respond with empty list" in {
       val getInvitations = HttpRequest(HttpMethods.GET, uri = "/invitation")
 
       getInvitations ~> invitationServiceRoute ~> check {
@@ -33,8 +34,67 @@ class InvitationServiceTest extends WordSpec with Matchers with ScalatestRouteTe
         val entityData = Unmarshal(response.entity).to[List[Invitation]]
         entityData.isCompleted shouldEqual true
         val returnedData = Await.result(entityData, 2 seconds)
-        returnedData should have size(0)
+        returnedData should have size (0)
+      }
+    }
+
+    "POST invitation with proper format should respond with success" in {
+      val invitationToAdd = Invitation("Domwos", "DomWos@wp.pl").toJson
+      val addNewInvitation = HttpRequest(HttpMethods.POST, uri = "/invitation",
+        entity = HttpEntity(MediaTypes.`application/json`, invitationToAdd.toString))
+
+
+      addNewInvitation ~> invitationServiceRoute ~> check {
+        status.isSuccess() shouldEqual true
+        val entityData = Unmarshal(response.entity).to[String]
+        entityData.isCompleted shouldEqual true
+        val returnedData = Await.result(entityData, 2 seconds)
+        returnedData shouldEqual "Invitation added Succesfully"
+      }
+    }
+
+    "POST invitation with proper format twice should respond with 'already added'" in {
+      val invitationToAdd = Invitation("Domwos", "DomWos@wp.pl").toJson
+      val addNewInvitation = HttpRequest(HttpMethods.POST, uri = "/invitation",
+        entity = HttpEntity(MediaTypes.`application/json`, invitationToAdd.toString))
+
+      addNewInvitation ~> invitationServiceRoute
+      addNewInvitation ~> invitationServiceRoute ~> check {
+        status.isSuccess() shouldEqual true
+        val entityData = Unmarshal(response.entity).to[String]
+        entityData.isCompleted shouldEqual true
+        val returnedData = Await.result(entityData, 2 seconds)
+        returnedData shouldEqual "Invitation was already added!"
+      }
+    }
+
+    "POST invitation with badly formatted email should respond with 'email format improper'" in {
+      val invitationToAdd = Invitation("DominWos", "ImproperMail").toJson
+      val addNewInvitation = HttpRequest(HttpMethods.POST, uri = "/invitation",
+        entity = HttpEntity(MediaTypes.`application/json`, invitationToAdd.toString))
+
+      addNewInvitation ~> invitationServiceRoute ~> check {
+        status.isFailure() shouldEqual true
+        val entityData = Unmarshal(response.entity).to[String]
+        entityData.isCompleted shouldEqual true
+        val returnedData = Await.result(entityData, 2 seconds)
+        returnedData shouldEqual "Email has improper format!"
+      }
+    }
+
+    "POST invitation with empty fields should respond with 'invitee or name empty'"  in {
+      val invitationToAdd = Invitation("", "ImproperMail").toJson
+      val addNewInvitation = HttpRequest(HttpMethods.POST, uri = "/invitation",
+        entity = HttpEntity(MediaTypes.`application/json`, invitationToAdd.toString))
+
+      addNewInvitation ~> invitationServiceRoute ~> check {
+        status.isFailure() shouldEqual true
+        val entityData = Unmarshal(response.entity).to[String]
+        entityData.isCompleted shouldEqual true
+        val returnedData = Await.result(entityData, 2 seconds)
+        returnedData shouldEqual "Invitee or Email is empty!"
       }
     }
   }
+
 }
